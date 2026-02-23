1d1abb0968004c9994cbd152df6a09c8961031ad
permjeetsingh16-byte <permjeetsingh16@gmail.com>
2026-02-23 21:38:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Fake login system
+function loginUser() {
+    alert("Login Successful (Demo Mode)");
+}
 
+// Course completion system
+function completeCourse() {
+    localStorage.setItem("webCourse", "completed");
+    updateProgress();
+    alert("Course Completed! 🎉");
+}
+
+function updateProgress() {
+    const progressBar = document.querySelector(".progress-bar");
+    if(localStorage.getItem("webCourse") === "completed") {
+        progressBar.style.width = "100%";
+    }
+}
+
+window.onload = function() {
+    updateProgress();
+};
